7d845c64072c4894836545c2d2363dcd262fe2bb
Prakhar Srivastav <prakhar1989@gmail.com>
2014-04-26 00:18:33 +0300

    Few more go examples

---
+++ rate_limiting.go
@@ -0,0 +1,42 @@
+package main
+
+import "time"
+import "fmt"
+
+func main() {
+
+	requests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		requests <- i
+	}
+	close(requests)
+
+	limiter := time.Tick(time.Millisecond * 500)
+
+	for req := range requests {
+		<-limiter // blocking
+		fmt.Println("request", req, time.Now())
+	}
+
+	burstyLimiter := make(chan time.Time, 3)
+
+	for i := 0; i < 3; i++ {
+		burstyLimiter <- time.Now()
+	}
+
+	go func() {
+		for t := range time.Tick(time.Millisecond * 500) {
+			burstyLimiter <- t
+		}
+	}()
+
+	burstyRequests := make(chan int, 5)
+	for i := 1; i <= 5; i++ {
+		burstyRequests <- i
+	}
+	close(burstyRequests)
+	for req := range burstyRequests {
+		<-burstyLimiter
+		fmt.Println("request", req, time.Now())
+	}
+}
---
+++ sorting.go
@@ -0,0 +1,35 @@
+package main
+
+import "sort"
+import "fmt"
+
+type ByLength []string // alias for builtin string type
+
+func (s ByLength) Len() int {
+	return len(s)
+}
+
+func (s ByLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s ByLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+func main() {
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	ints := []int{7, 5, 10, 312}
+	sort.Ints(ints)
+	fmt.Println("Ints: ", ints)
+
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted: ", s)
+
+	fruits := []string{"peach", "banana", "kiwi"}
+	sort.Sort(ByLength(fruits)) // using a custom function to sort
+	fmt.Println(fruits)
+}
